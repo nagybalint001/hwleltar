7a07bde0278b841b1792d17a9c13e31b47d2dabb
Nagy Bálint <balint.boy@gmail.com>
2018-02-08 20:24:18 +0100

    Create Item model

---
+++ backend/backend/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Manufacturer { get; set; }
+        public int Price { get; set; }
+    }
+}
