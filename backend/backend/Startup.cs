9e5f9b653d068c0231120a8ffa0d59cbb202f5e4
Nagy Bálint <balint.boy@gmail.com>
2018-02-08 21:38:42 +0100

    Enable CORS

--- backend/backend/Startup.cs
+++ backend/backend/Startup.cs
@@ -18,6 +18,12 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyCorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddDbContext<ItemContext>(opt => opt.UseInMemoryDatabase("ItemList"));
             services.AddMvc();
         }
@@ -25,6 +31,7 @@ namespace backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyCorsPolicy");
             app.UseMvc();
         }
     }
