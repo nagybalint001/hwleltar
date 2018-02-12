588b0a9897b745db2f766f2b51358239e6b5f7b8
Nagy Bálint <balint.boy@gmail.com>
2018-02-12 10:05:17 +0100

    Extra fields basic implementation

--- frontend/src/app/details/details.component.ts
+++ frontend/src/app/details/details.component.ts
@@ -12,6 +12,7 @@ import { Item } from '../item';
 export class DetailsComponent implements OnInit {
   params: {type:string, id:number};
   item: Item;
+  extras: any;
 
   constructor(
     private itemService: ItemService,
@@ -19,15 +20,35 @@ export class DetailsComponent implements OnInit {
     private router:Router,
     private location:Location) {
     this.route.params.subscribe(res => { this.params = {type:res.type, id:res.id}});
+    this.extras = {};
   }
 
   ngOnInit() {
     if(this.params.id){
-      this.itemService.getItem(this.params.id).subscribe(item => this.item = item);
+      this.itemService.getItem(this.params.id).subscribe(item => {
+        this.item = item;
+        this.loadExtras();
+      });
     } else {
       this.item = new Item();
       this.item.type = this.params.type;
     }
+  }
+
+  loadExtras() {
+    if(this.item.type.toUpperCase() == "VGA"){
+      let extras = JSON.parse(this.item.extras);
+      this.extras.memorytype = extras.memorytype;
+      this.extras.memory = extras.memory;
+      this.extras.displayport = extras.displayport;
+    }
+    else if(this.item.type.toUpperCase() == "CPU"){
+      let extras = JSON.parse(this.item.extras);
+      this.extras.socket = extras.socket;
+      this.extras.threads = extras.threads;
+      this.extras.frequency = extras.frequency;
+    }
+
   }
 
   uploadImage(){
@@ -39,6 +60,17 @@ export class DetailsComponent implements OnInit {
   }
 
   onSubmit(id, data){
+    if(this.item.type.toUpperCase() == "VGA") {
+      data.extras = JSON.stringify({memorytype: data.memorytype, memory: data.memory, displayport: data.displayport})
+      delete data.memorytype;
+      delete data.memory;
+      delete data.displayport;
+    } else if (this.item.type.toUpperCase() == "CPU"){
+      data.extras = JSON.stringify({socket: data.socket, threads: data.threads, frequency: data.frequency})
+      delete data.socket;
+      delete data.threads;
+      delete data.frequency;
+    }
     if(id){
       this.itemService.updateItem(id, data, () => this.location.back()).subscribe();
     } else {
